3bf940f921b763fcdfc7b6d70babb44a7e721a25
ramyapentyala <ramyapentyala@gmail.com>
2022-02-03 20:35:59 -0600

    commit

--- public/viewpage/home_page.js
+++ public/viewpage/home_page.js
@@ -1,11 +1,26 @@
 import * as Elements from './elements.js'
 import { routePathnames } from '../controller/route.js';
 import { currentUser } from '../controller/firebase_auth.js';
+
+let imageFile2Upload = null;
+
 export function addEventListeners() {
     Elements.menuHome.addEventListener('click', () => {
         history.pushState(null, null, routePathnames.HOME);
         home_page();
     });
+    Elements.formAddProduct.imageButton.addEventListener('change', e => {
+        imageFile2Upload = e.target.files[0];
+        if (!imageFile2Upload) {
+            Elements.formAddProduct.imageTag.removeAttribute('src');
+            return;
+            
+        }
+        const reader = new FileReader();
+        reader.readAsDataURL(imageFile2Upload);
+        reader.onload = () => Elements.formAddProduct.imageTag.src = reader.result;
+    });
+    Elements.formAddProduct.form.addEventListener('submit', addNewProduct);
 }
 
 export function home_page() {
@@ -22,4 +37,10 @@ export function home_page() {
     `;
 
     Elements.root.innerHTML = html;
+}
+function addNewProduct(e) {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const price = e.target.price.value;
+    const summary = e.target.summary.value;
 }
